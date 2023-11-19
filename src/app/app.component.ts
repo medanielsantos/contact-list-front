9badf74483c17aeabf694fd299b581923088679e
Daniel Henrique <medanielsantos@gmail.com>
2023-11-19 00:45:13 -0300

    inital base front with datatable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,47 @@
-import { Component } from '@angular/core';
+import {OnInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 import {environment} from "../environments/environment";
 import {PersonService} from "./services/person.service";
 import {Person} from "./models/person.model";
+import {MatSort} from "@angular/material/sort";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'contact-app';
+export class AppComponent implements OnInit{
+  displayedColumns: string[] = [
+    'is_favorite',
+    'name',
+    'actions'
+  ];
 
-  people: Person[] = [];
+  dataSource!: MatTableDataSource<Person>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private personService: PersonService) {
+    this.getPersonList();
+  }
+
+  ngOnInit() {
     this.getPersonList();
   }
 
   getPersonList() {
     this.personService.getPerson().subscribe({
       next: (res) => {
-        this.people = res.data;
+        this.dataSource = new MatTableDataSource<any>(res.data);
+        this.dataSource.paginator = this.paginator;
+
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+
+        console.log(this.dataSource);
       },
       error: (error) => {
         console.log(error);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -13,6 +19,12 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
